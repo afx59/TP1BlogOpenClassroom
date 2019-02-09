18967fb6a1b480998d29d0165a2b5c2bdd07e406
afx59 <x.flipo@yahoo.com>
2019-02-09 11:13:36 +0100

    Commit de nouveaux composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tp1-blog-oc';
+  listPost : Array<Post> = [
+    new Post("Mon premier post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at posuere ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nisi lorem, fringilla sit amet mi sed, gravida malesuada neque. Duis nibh nunc, rutrum eget sapien rutrum, facilisis sollicitudin magna.",0),
+    new Post("Mon deuxième post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at posuere ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nisi lorem, fringilla sit amet mi sed, gravida malesuada neque. Duis nibh nunc, rutrum eget sapien rutrum, facilisis sollicitudin magna.",0),
+    new Post("Mon troisième post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at posuere ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nisi lorem, fringilla sit amet mi sed, gravida malesuada neque. Duis nibh nunc, rutrum eget sapien rutrum, facilisis sollicitudin magna.",0)
+  ];
 }
